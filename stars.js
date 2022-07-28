5b248c3b39ca56d214722a71fe821aeb114ed318
Luisjor <108550000+Luisjor@users.noreply.github.com>
2022-07-28 11:11:40 -0600

    Add files via upload

---
+++ stars.js
@@ -0,0 +1,44 @@
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', ready)
+} else{
+    GetRating()
+    Submit()
+}
+
+function GetRating() {
+    var NumStars = document.getElementsByClassName("BotonesRating")
+    var texto = ""
+    for (var i = 0; i < NumStars.length; i++) {
+        var button = NumStars[i];
+        button.addEventListener("click", function(event) {
+            var buttonClicked = event.target.innerText
+            texto = `You selected ${buttonClicked} out of 5`
+            Submit(texto)
+        })
+        }
+        
+    }
+
+function Submit(texto) {
+    var Submitting = document.getElementsByClassName("SubmitButton")
+    for (var i = 0; i < Submitting.length; i++) {
+            var SubmitBut = Submitting[i]
+            SubmitBut.addEventListener("click", function(event) {
+                ChangeHTML(texto)
+            })
+    }
+}
+
+function ChangeHTML(texto) {
+        var Container = document.getElementById("Container")
+        var ThankYouContainer = 
+            `<div id="ThankYouBox">
+            <img src="C:/Users/Luisjor/Desktop/Challenge/interactive-rating-component-main/images/illustration-thank-you.svg" class="ThankYouImg">
+            <span class="Calificacion">${texto}</span>
+            <h1>Thank you!</h1>
+            <span class="centered">
+                We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!
+            </span>
+            </div>`
+        Container.innerHTML = ThankYouContainer
+       }
